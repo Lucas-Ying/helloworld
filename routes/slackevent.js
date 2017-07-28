ad54ab395fcf1a0a287864346c58ca5ec2bd3e4c
Lucas Ying <lucas.yingmm@gmail.com>
2017-07-28 14:43:33 +1200

    added 3 seconds delay before start connection

--- routes/slackevent.js
+++ routes/slackevent.js
@@ -25,22 +25,25 @@ router.get('/', function(req, res, next) {
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var bot_token = token;
-var rtm = new RtmClient(bot_token);
+
+setTimeout(function() {
+    var bot_token = token;
+    var rtm = new RtmClient(bot_token);
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
-});
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+        console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+    });
 
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-    if (message.attachments) {
-        connection.sseSend(message.attachments[0].text);
-    } else {
-        connection.sseSend(message.text);
-    }
-});
+    rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+        if (message.attachments) {
+            connection.sseSend(message.attachments[0].text);
+        } else {
+            connection.sseSend(message.text);
+        }
+    });
 
-rtm.start();
+    rtm.start();
+}, 3000);
 
 module.exports = router;
